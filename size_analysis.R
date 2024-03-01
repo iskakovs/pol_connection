16db321c9b3a1f6875c84bec362cfbd065e31af8
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-03-02 00:25:21 +0600

    Update size_analysis.R

--- size_analysis.R
+++ size_analysis.R
@@ -5,3 +5,8 @@ library(stargazer)
 # Install and load the writexl package 
 install.packages("writexl")
 library(writexl)
+
+# Step 1: Read the dataset from Excel
+library(readxl)
+
+data <- read_excel("C:\\Users\\777\\Desktop\\pol_data.xlsx", sheet = "data")
