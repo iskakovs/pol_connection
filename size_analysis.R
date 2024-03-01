200b41c4dca6ddc4ecd93e80fad79bc0889ecbd3
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-03-02 00:26:08 +0600

    Update size_analysis.R

--- size_analysis.R
+++ size_analysis.R
@@ -9,4 +9,4 @@ library(writexl)
 # Step 1: Read the dataset from Excel 
 library(readxl)
 
-data <- read_excel("C:\\Users\\777\\Desktop\\pol_data.xlsx", sheet = "data")
+data <- read_excel("...\\pol_data.xlsx", sheet = "data")
