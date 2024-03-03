8c59ad56705606c734100a31c1e9ee04deb0aee2
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-03-03 07:38:32 +0600

    Update size_analysis.R
    Calculate company age

--- size_analysis.R
+++ size_analysis.R
@@ -14,3 +14,6 @@ data <- read_excel("...\\pol_data.xlsx", sheet = "data")
 # Step 2: Prepare the data
 # Convert reg_date to Date format 
 data$reg_date <- as.Date(data$reg_date, format = "%d.%m.%Y")
+
+# Calculate company age
+data$age <- as.numeric(format(Sys.Date(), "%Y")) - data$year
