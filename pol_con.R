d12e0ad353a8bf8fab89a947a23c01defeb65854
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-03-05 00:16:06 +0600

    Summary statistics for numeric variables

--- pol_con.R
+++ pol_con.R
@@ -8,3 +8,5 @@ pol_data <- read_excel("...pol_data2.xlsx", sheet = "data")
 ############# Summary Statistics ##################
 # Filter the dataset when pol variable is equal to 1
 pol_data_pol_1 <- pol_data[pol_data$pol == 1, ]
+
+# Summary statistics for numeric variables
