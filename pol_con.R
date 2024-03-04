3d9eb0031545ec22e806d35a2fc2b43f34431cf7
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-03-05 00:20:56 +0600

    Update pol_con.R

--- pol_con.R
+++ pol_con.R
@@ -19,4 +19,4 @@ summary_stats_categorical <- lapply(pol_data_pol_1, table)
 summary_stats_categorical_df <- as.data.frame(do.call(cbind, summary_stats_categorical))
 
 # Make sure the number of rows match
-min_rows
+min_rows <- min(nrow(summary_stats_numeric), nrow(summary_stats_categorical_df))
